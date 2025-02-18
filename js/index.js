39383be68de0a6da1eb3064471340924b4953a3e
Pavithrank02 <pavithrank02@gmail.com>
2025-02-18 14:10:08 +0530

    stcked

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 let text = "i need to reverse this text";
-let words = text.split(" ")
-// console.log(words)
-for(i = 0; i < words.length; i++){
-    let rev_word = ""
-    for(j= words[i].length - 1; j>=0; j--){
-        rev_word += words[i][j] 
+let words = text.split(" "); // Split the text into words
+
+for (let i = 0; i < words.length; i++) {
+    let st = [];  // Initialize an empty array to store the reversed characters
+    let rev_word = ""; 
+    let final = [] // Initialize an empty string to store the reversed word
+    
+    // Loop through each character of the word in reverse order
+    for (let j = words[i].length - 1; j >= 0; j--) {
+        rev_word += words[i][j];  // Add each character to the reversed word string
+        
+        
     }
-    console.log(rev_word)
+    final.push(rev_word)
+    // Add the reversed word to the array
+    console.log(final.join(" "))
+  
 }
+
