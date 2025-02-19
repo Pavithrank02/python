9c109e35f7569132f2551526496b46ff5aaf4e47
Pavithrank02 <pavithrank02@gmail.com>
2025-02-19 14:21:02 +0530

    comments

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
-let text = "i need to reverse this text";
-let words = text.split(" "); // Split the text into words
+// let text = "i need to reverse this text";
+// let words = text.split(" "); // Split the text into words
 
-for (let i = 0; i < words.length; i++) {
-    let st = [];  // Initialize an empty array to store the reversed characters
-    let rev_word = ""; 
-    let final = [] // Initialize an empty string to store the reversed word
+// for (let i = 0; i < words.length; i++) {
+//    // Initialize an empty array to store the reversed characters
+//     let rev_word = ""; 
+//     let final = [] // Initialize an empty string to store the reversed word
     
-    // Loop through each character of the word in reverse order
-    for (let j = words[i].length - 1; j >= 0; j--) {
-        rev_word += words[i][j];  // Add each character to the reversed word string
+//     // Loop through each character of the word in reverse order
+//     for (let j = words[i].length - 1; j >= 0; j--) {
+//         rev_word += words[i][j];  // Add each character to the reversed word string
         
         
-    }
-    final.push(rev_word)
-    // Add the reversed word to the array
-    console.log(final.join(" "))
+//     }
+//     final.push(rev_word)
+//     // Add the reversed word to the array
+//     console.log(final.join(" "))
   
-}
+// }
+
+
+
 
